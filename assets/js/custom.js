9557d2418754d5347c146cf3b8b5993b80c811b1
Jiho Choi <jihochoi1993@gmail.com>
2020-01-12 19:21:39 +0900

    Update the temporal project section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,30 @@
 // preloader
-setTimeout(function(){
-    $('.preloader').css('display', 'none');
+setTimeout(function() {
+  $(".preloader").css("display", "none");
 }, 2000);
 
-$(window).load(function () {
-    $('.preloader').fadeOut(1000); // set duration in brackets
+$(window).load(function() {
+  $(".preloader").fadeOut(1000); // set duration in brackets
 });
 
-$(function () {
-    new WOW().init();
-    $('.main-nav').singlePageNav({
-        offset: 70
-    });
+$(function() {
+  new WOW().init();
+  $(".main-nav").singlePageNav({
+    offset: 70
+  });
 
-    /* Hide mobile menu after clicking on a link
+  /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function () {
-        $(".navbar-collapse").collapse('hide');
-    });
-})
+  $(".navbar-collapse a").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+});
+
+$("#down").click(function() {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#about").offset().top - 100
+    },
+    500
+  );
+});
